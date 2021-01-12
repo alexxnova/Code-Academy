627c03f4be7e78f5d8897bbc3bf9e7d487be1743
Aleksandra Ivanova <32313104+alexxnova@users.noreply.github.com>
2021-01-12 15:50:08 +0200

    Update 20210111_9.c

--- 20210111_9.c
+++ 20210111_9.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-int evenZeros(int num);
+int rangeZeros(int num, int posMax, int posMin);
 
 int main() {
-    evenZeros(171);
+    rangeZeros(171, 7 , 3);
     return 0;
 }
 
-int evenZeros(int num){
-    int mask=7;
+int rangeZeros(int num, int posMax, int posMin){
+    int mask=0xFFFFFFFF;
+    mask=mask>>(posMax+posMin+1);
+    mask=~(mask<<posMin);
     int ans=num&mask;
     printf("%d",ans);
     return 0;
