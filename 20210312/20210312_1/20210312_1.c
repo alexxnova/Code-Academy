3b7e7b00cc19e498e0c3ea572d96b5c8db25dbbc
Aleksandra Ivanova <32313104+alexxnova@users.noreply.github.com>
2021-03-13 15:24:32 +0200

    Update 20210312_1.c

--- 20210312/20210312_1/20210312_1.c
+++ 20210312/20210312_1/20210312_1.c
@@ -1,1 +1,33 @@
 
+/* Задача 1. Реализация на списък. Напишете програма, която добавя и
+изтрива елемент от списък по позиция в списъка. Използвайте следния
+елемент на динамичния списък: */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+node_t * head;
+
+int main(void){
+    init();
+    for (int i=0; i<10;i++){
+        add(i);
+    }
+    printList();
+    int x;
+    
+    dellFirst(&x);
+    printf("\n");
+    printList();
+
+    dellEnd(&x);
+    printf("\n");
+    printList();
+    
+    dellElemReturnAfter(&x, 9);
+    printf("\n");
+    printList();
+
+    return 0;
+}
